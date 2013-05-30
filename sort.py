f47d01e9675b80bcf6b7bdb427051002f70faf85
luchux <lucianoguasco@gmail.com>
2013-05-30 14:42:03 +1000

    Insertion sort added

--- sort.py
+++ sort.py
@@ -1,13 +1,25 @@
 # n-1 passing. O(n^2)
 def bubble_sort(alist):
-    for passing in range(len(alist)-1, 0, -1):
-        for i in range(passing):
+    for index in range(len(alist)-1, 0, -1):
+        for i in range(index):
             if alist[i] > alist[i+1]:
                 alist[i], alist[i+1] = alist[i+1], alist[i]
 
+def insertion_sort(alist):
+    for index in range(1,len(alist)):
+
+        actual = alist[index]
+        position = index
+
+        while position > 0 and actual < alist[position-1]:
+            alist[position] = alist[position-1]
+            position -= 1
+
+        alist[position] = actual
+
 def main():
     alist = [4, 6, 1, 2, 5, 3]
-    bubble_sort(alist)
+    insertion_sort(alist)
     print alist
 
 if __name__ == '__main__':
