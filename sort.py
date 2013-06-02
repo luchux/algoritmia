ff8766e934f1b00aabf02f56f945178d9fe9421f
luchux <lucianoguasco@gmail.com>
2013-06-02 19:01:42 +1000

    QuickSort added

--- sort.py
+++ sort.py
@@ -50,11 +50,48 @@ def merge_sort(alist):
             iright += 1
             imerg += 1
 
+#partition function to find the pivot in QuickSort
+def partition(alist, first, last):
+    pivotval = alist[first]
+
+    leftpter = first+1
+    rightpter = last
+
+    done = False
+    while not done:
+        while leftpter <= rightpter and alist[leftpter] <= pivotval:
+            leftpter += 1
+
+        while leftpter <= rightpter and alist[rightpter] >= pivotval:
+            rightpter -= 1
+
+        if rightpter < leftpter:
+            done = True
+
+        else:
+            alist[leftpter], alist[rightpter] =  alist[rightpter], alist[leftpter]
+
+    alist[first], alist[rightpter] = alist[rightpter], alist[first]
+    return rightpter
+
+
+#it does the actual work, of call partition, and then the recursive calls.
+def quick_sort_helper(alist, first, last):
+    if first < last:
+
+        pivot = partition(alist, first, last)
+
+        quick_sort_helper(alist, first, pivot-1)
+        quick_sort_helper(alist, pivot+1, last)
+
+#wrapper
+def quick_sort(alist):
+    quick_sort_helper(alist, 0, len(alist)-1)
 
 
 def main():
     alist = [4, 6, 1, 2, 5, 3]
-    merge_sort(alist)
+    quick_sort(alist)
     print alist
 
 if __name__ == '__main__':
